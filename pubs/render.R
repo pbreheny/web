5753b34179321fa099043ab1bb2b166533148476
Patrick Breheny <patrick-breheny@uiowa.edu>
2020-06-05 12:00:48 -0500

    pubs/x.r: Rewritted to be more modular, now uses render()

---
+++ pubs/render.R
@@ -0,0 +1,18 @@
+# Note: Must have D1, D2 in global env
+render <- function(infile, outfile) {
+  content <- readLines(infile)
+  if (content[length(content)]!="") {
+    writeLines(paste(infile, "must end with blank line"), con="/dev/tty")
+    stop()
+  }
+  repeat {
+    needsReplaced <- grep("^_", content)
+    if (length(needsReplaced)==0) break
+    i <- needsReplaced[1]
+    ID <- gsub("_", "", content[i])
+    pre <- content[1:(i-1)]
+    post <- content[(i+1):length(content)]
+    content <- c(pre, htmlExport(ID), post)
+  }
+  writeLines(content, con="../publications.html")
+}
--- pubs/x.R
+++ pubs/x.R
@@ -1,35 +1,6 @@
 source("htmlExport.R")
+source("render.R")
 D1 <- read.csv("cv.csv", stringsAsFactors=FALSE)
 D2 <- read.delim("cv-extra.txt", stringsAsFactors=FALSE)
-
-pubs <- readLines("pubs.html")
-if (pubs[length(pubs)]!="") {
-  writeLines(paste("pubs.html must end with blank line"), con="/dev/tty")
-  stop()
-}
-repeat {
-  needsReplaced <- grep("^_", pubs)
-  if (length(needsReplaced)==0) break
-  i <- needsReplaced[1]
-  ID <- gsub("_", "", pubs[i])
-  pre <- pubs[1:(i-1)]
-  post <- pubs[(i+1):length(pubs)]
-  pubs <- c(pre, htmlExport(ID), post)
-}
-writeLines(pubs, con="../publications.html")
-
-res <- readLines("res.html")
-if (res[length(res)]!="") {
-  writeLines(paste("res.html must end with blank line"), con="/dev/tty")
-  stop()
-}
-repeat {
-  needsReplaced <- grep("^_", res)
-  if (length(needsReplaced)==0) break
-  i <- needsReplaced[1]
-  ID <- gsub("_", "", res[i])
-  pre <- res[1:(i-1)]
-  post <- res[(i+1):length(res)]
-  res <- c(pre, htmlExport2(ID), post)
-}
-writeLines(res, con="../research.html")
+render("pubs.html", "../publications.html")
+render("res.html", "../research.html")
